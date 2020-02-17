0079aeb78980d4fe8810fa7f613558f65d2e5399
Tim Allen <Timjallen.ta@gmail.com>
2020-02-17 02:14:24 -0800

    Most of the skeleton completed

--- src/App.js
+++ src/App.js
@@ -4,13 +4,47 @@ import logo from './images/logo.webp';
 
 function App() {
 	return (
-		<div className='App'>
+		<div>
+			<div className='Page'>
+				<div className='Banner Margin'>
+					<h1 className='PurpleTextThingy'>be original</h1>
+				</div>
+				<div className='Body Margin'>
+					<div className='Products'>
+						<div className='Items'>
+							<Item name='prod 1' />
+							<Item name='prod 2' />
+							<Item name='prod 3' />
+						</div>
+					</div>
+
+					<div className='HowItWorksSpacer' />
+					<div className='HowItWorks'>
+						HOW DOES IT WORK?<br />we're not exactly sure
+					</div>
+
+					<div className='OrderTodaySpacing' />
+					<div className='OrderToday'>
+						<h1 className='PurpleTextThingy'>ORDER TODAY</h1>
+					</div>
+
+					<div className='ReviewSpacing' />
+					<div className='Review'>
+						<h1>WHAT SNAXXER ARE SAYING</h1>
+						<p>we make the big yummy</p>
+					</div>
+				</div>
+				<div className='Footer Margin'>hi nick</div>
+			</div>
 			<div className='Header'>
 				<img src={logo} alt='' className='logo' height='100%' />
 			</div>
-			page
 		</div>
 	);
 }
+
+const Item = ({ name, url, img }) => {
+	return <div className='Item'>{name}</div>;
+};
 
 export default App;
