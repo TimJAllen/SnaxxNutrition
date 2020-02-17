a438eeca34a6e964312d3b419aa6cc7bb9c3f725
Tim Allen <Timjallen.ta@gmail.com>
2020-02-16 16:40:54 -0800

    Clean up code and first push to Amplify test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import logo from './images/logo.webp';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div className='Header'>
+				<img src={logo} alt='' className='logo' height='100%' />
+			</div>
+			page
+		</div>
+	);
 }
 
 export default App;
